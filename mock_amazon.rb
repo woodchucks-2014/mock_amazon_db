73153f98f4afee6da4b7120738b3e9560657f2c1
adriansoghoian <adriansoghoian@gmail.com>
2014-06-13 15:44:55 -0400

    Updated USERS and PUBLISHERS tables

--- mock_amazon.rb
+++ mock_amazon.rb
@@ -14,4 +14,40 @@
 require "sqlite3"
 require "faker"
 
+database = SQLite3::Database.open "database.db"
+
+fake_companies = []
+fake_names = []
+email = []
+password = []
+username = []
+
+100.times do 
+	# fake_names << Faker::Name.name 
+	fake_companies << Faker::Company.name
+	# email << Faker::Internet.email
+	# password << Faker::Internet.password
+	# username << Faker::Internet.user_name
+end
+# p fake_names
+# p email
+# p password
+# p username
+# p fake_companies
+
+## UPDATING USERS
+# for i in 0...100
+# 	database.execute("INSERT INTO users (name, email, password, username, updated_at, created_at) VALUES (?, ?, ?, ?, datetime('now'), datetime('now'))",
+# 	 [fake_names[i], email[i], password[i], username[i]])
+# end
+
+## UPDATING PUBLISHERS
+# for i in 0...100
+# 	database.execute("INSERT INTO publishers (name, updated_at, created_at) VALUES (?, datetime('now'), datetime('now'))",
+# 	 [fake_companies[i]])
+# end
+# p database.execute("SELECT * FROM publishers")
+
+
+## UPDATING 
 
