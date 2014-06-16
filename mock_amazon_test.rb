b7e70f912409e57341c860f882020d3a0032aa41
Tanner Welsh <tannerwelsh@gmail.com>
2014-06-16 16:08:20 -0400

    Move data seeding into Rake task (db:seed); leave testing script as mock_amazon_test.rb

---
+++ mock_amazon_test.rb
@@ -0,0 +1,25 @@
+require "sqlite3"
+
+database = SQLite3::Database.open "amazon.db"
+
+## TESTS
+
+## Find all books published by X publisher
+
+database.execute("SELECT * FROM books")
+database.execute("SELECT * FROM reviews")
+database.execute("SELECT * FROM orders")
+database.execute("SELECT * FROM users")
+database.execute("SELECT * FROM authors")
+database.execute("SELECT * FROM publishers")
+
+author_ids = database.execute("SELECT id FROM authors;")
+test_author = author_ids[5]
+puts database.execute("SELECT * FROM books WHERE author_id = ?", test_author)
+
+
+## Find all reviews on a certain book
+## Find all reviews by a certain user
+## Find all books written by a certain author
+## Find all reviews on book X with rating > 4
+
